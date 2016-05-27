a43c50aba9bbead92cb7001383f1137381e608a3
mastermay <986492616@qq.com>
2016-05-27 17:13:01 +0800

    update

--- functions/articles.php
+++ functions/articles.php
@@ -47,4 +47,13 @@ function postformat_gallery(){
 	}
 }
 
+add_filter ('the_content', 'lazyload');
+function lazyload($content) {
+    $loadimg_url=get_bloginfo('template_directory').'/loading.gif';
+    if(!is_feed()||!is_robots) {
+        $content=preg_replace('/<img(.+)src=[\'"]([^\'"]+)[\'"](.*)>/i',"<img\$1data-original=\"\$2\" src=\"$loadimg_url\"\$3>\n<noscript>\$0</noscript>",$content);
+    }
+    return $content;
+}
+
  ?>
